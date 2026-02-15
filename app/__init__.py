580dbd319002f6c3e50f1d35982806b5bf464112
lupxg <gerson.chachagua80@gmail.com>
2026-02-15 15:14:20 -0600

    Change folder name to app

---
+++ app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from app.extensions import db
+from app.config import DevelopmentConfig
+
+def create_app():
+    app  = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
+    db.init_app(app)
+
+    from app.posts.post import post
+
+    app.register_blueprint(post)
+
+    return app
