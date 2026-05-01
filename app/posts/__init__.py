e48f4ac8ecdb4714f8013888942901ea43ebecbb
lupxg <gerson.chachagua80@gmail.com>
2026-05-01 15:36:58 -0600

    Change file name

--- app/posts/__init__.py
+++ app/posts/__init__.py
@@ -1,6 +1,6 @@
 BASE_ROUTE = 'posts'
 
 def register_route(api, app, root='api'):
-    from .controller import api as postapi
+    from .endpoint import api as postapi
 
     api.add_namespace(postapi, path=f'/{root}/{BASE_ROUTE}') 
